59af64b7164c53b41469e465caf7288ab5883d6c
oliviachidi <oliviachidi5@gmail.com>
2022-07-29 06:48:37 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".side-bar");
+
+menuIcon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+}
